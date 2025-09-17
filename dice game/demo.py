1a620feb84ec3a772660a747fc23b6fc45c328e3
vikramsnikam <your_email@example.com>
2025-09-17 15:40:32 +0530

    worked on dev

--- dice game/demo.py
+++ dice game/demo.py
@@ -1,13 +1,25 @@
-from mainfunction import display
+print('calculator using function')
+def add(a,b):
+     print(float(a) + float(b))
+def sub(a,b):
+    print(float(a) - float(b))
+def mult(a,b):
+    print(float(a) * float(b))
+def div(a,b):
+    print(float(a) / float(b))
+def power(a,b):
+    print(float(a) **float(b))
+a = input('enter a number')
+b = input('enter a second number')
+c = input('enter a operation')
 
-display()
-
-
-# a=7/0
-
-
-b=10
-if (b==10):
-    raise ValueError('wrong value')
-else:
-    print('ok')
+if c=='+':
+    add(a,b)
+if c=='-':
+    sub(a,b)
+if c=='*':
+    mult(a,b)
+if c=='/':
+    div(a,b)
+if c=='**':
+    power(a,b)
